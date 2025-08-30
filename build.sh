cc7005f43ac70ec74ce062cf7d97484c8d739b87
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-30 10:01:28 +0000

    build.sh: created to simplify/unify cms building stage

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Date:   Sat Aug 30 09:40:48 AM UTC 2025
+# Issue:  https://github.com/sfmunoz/cms/issues/13
+#
+
+set -e
+
+cd "$(dirname "$0")"
+
+[ "$HUGO_THEME" = "" ] && HUGO_THEME="headless"
+export HUGO_THEME
+
+set -x
+
+# cleanup: hugo doesn't do it
+rm -rf public
+hugo build --gc --minify "$@"
+
